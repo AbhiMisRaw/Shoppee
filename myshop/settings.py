62bef9b27603d329a8ceac8ae5a8e193d812db6b
Abhishek Mishra <abhishekmishra@Abhisheks-MacBook-Pro.local>
2025-01-27 22:58:26 +0530

    Added Cart functionality by using django-session

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    "cart.apps.CartConfig"
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "cart.context_processors.cart",
             ],
         },
     },
@@ -81,6 +83,7 @@ DATABASES = {
     }
 }
 
+CART_SESSION_ID = "cart"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
